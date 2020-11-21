80d0bf719cfe2064fd60c0a63f07caa38d3402cd
Han <hanzh.xu@gmail.com>
2020-11-21 15:52:11 -0800

    Update macro_yield.cpp

--- assets/code/cpp/macro_yield.cpp
+++ assets/code/cpp/macro_yield.cpp
@@ -58,28 +58,30 @@ default: { isAlive = false; return true; } }
 #define IF(name,c,a,b) \
 case BEG(name): { if(c) { {a;} state = END(name); return false; } else { state = ELSE(name); } } \
 case ELSE(name): { b; } \
-case END(name): { state = END(name); }
+case END(name): {}
 
 #define LOOP(name,s) \
 case BEG(name): { {s;} state = BEG(name); return false; } \
-case END(name): { state = END(name); }
+case END(name): {}
 
 #define WHILE(name,c,s) \
 case BEG(name): { if(!(c)) { state = END(name); return false; } {s;} state = BEG(name); return false; } \
-case END(name): { state = END(name); }
+case END(name): {}
 
 #define YIELD(name) \
 case BEG(name): { state = END(name); isAlive = true; return true; } \
 case END(name): {}
 
+#define CONTINUE(loop) { state = BEG(loop); return false; }
 #define BREAK(loop) { state = END(loop); return false; }
 
-#define DEC_BEG enum { BEG,
+#define GOTO(label) { state = label; return false; }
+
+#define DEC_BEG enum { BEG = 0,
 #define DEC_IF(name) BEG(name), ELSE(name), END(name)
 #define DEC_LOOP(name) BEG(name), END(name)
 #define DEC_YIELD(name) BEG(name), END(name)
 #define DEC_END };
-  
 
 /*
 L: def prod_iter(s):
@@ -109,7 +111,9 @@ public:
     OneMoreProdGen(const vector<unsigned int>& _s) : s(_s) {}
     bool step(vector<int>& output) {
         DEC_BEG
-            DEC_IF(if1), DEC_IF(if2), DEC_IF(if3), DEC_LOOP(loop1), DEC_LOOP(loop2), DEC_YIELD(y1), DEC_YIELD(y2)
+            DEC_IF(if1), DEC_IF(if2), DEC_IF(if3),
+            DEC_LOOP(loop1), DEC_LOOP(loop2),
+            DEC_YIELD(y1), DEC_YIELD(y2)
         DEC_END
         GEN_BEG
         IF(if1, s.size()==0, { 
@@ -125,9 +129,9 @@ public:
                 }
                 WHILE(loop2, iter->next(xs), {
                     output.clear();
-                        output.push_back(x);
-                        output.insert(output.end(), xs.begin(), xs.end());
-                        YIELD(y2);
+                    output.push_back(x);
+                    output.insert(output.end(), xs.begin(), xs.end());
+                    YIELD(y2);
                 });
                 x += 1;
             })
@@ -155,7 +159,8 @@ public:
     int n;
     string a, b, c;
     shared_ptr<Gen<string> > iter;
-    OneMoreHanoiGen(int _n, string _a, string _b, string _c) : n(_n), a(_a), b(_b), c(_c) {}
+    OneMoreHanoiGen(int _n, string _a, string _b, string _c):
+        n(_n), a(_a), b(_b), c(_c) {}
     bool step(string& output) {
         DEC_BEG
             DEC_IF(if1), DEC_LOOP(loop1), DEC_LOOP(loop2), DEC_LOOP(loop3), 
@@ -177,6 +182,32 @@ public:
     }
 };
 
+class PrimeGen : public Gen<int> {
+public:
+    vector<int> primes;
+    int i, j;
+    PrimeGen() {}
+    bool step(int& output) {
+        DEC_BEG
+            DEC_IF(if1), DEC_IF(if2),
+            DEC_LOOP(loop1), DEC_LOOP(loop2),
+            DEC_YIELD(y1)
+        DEC_END
+        GEN_BEG
+        i = 2;
+        WHILE(loop1, true, {
+            j = 0;
+            WHILE(loop2, j < primes.size(), {
+               IF(if1, i % primes[j] == 0, {i++; CONTINUE(loop1);}, j++); 
+            });
+            primes.push_back(i); 
+            output = i; 
+            YIELD(y1);
+        });
+        GEN_END
+    }
+};
+
 int main() {
     cout << "HanoiGen" << endl;
     string s;
@@ -189,5 +220,13 @@ int main() {
     OneMoreProdGen prodGen(dimSize);
     while(prodGen(output)) print(output);
     
+    cout << "Prime numbers" << endl;
+    PrimeGen primeGen;
+    int p;
+    for(int i = 0; i < 30; ++i) {
+        primeGen(p);
+        cout << p << " ";
+    }
+    cout << endl;
     return 0;
 }
