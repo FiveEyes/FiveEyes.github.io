4f7088e9d00a3ad868d8d131d0cb0fd04b1d32f0
Han <hanzh.xu@gmail.com>
2020-11-21 20:46:21 -0800

    Update macro_yield.cpp

--- assets/code/cpp/macro_yield.cpp
+++ assets/code/cpp/macro_yield.cpp
@@ -86,10 +86,10 @@ int main() {
     return 0;
 }
 
-// You also can create a coroutine having full functionality, if you want to do some interactive operations with the coroutine.
-// The only difference is that the step method has an extra argument input now.
-// The input argument represents the input variable that you pass into the Coroutine.
-// See the example class GuessNumber to understand how to use it.
+// You are also able to create a coroutine having full functionality, if you want to do some interactive operations with the coroutine.
+// The only difference from the generator class is that the step method has an extra argument `input` now.
+// The argument `input` represents the input variable that you pass into the Coroutine.
+// See the example class `GuessNumber` to understand how it works.
 template<typename S, typename T>
 class Coroutine : public std::enable_shared_from_this<Coroutine<S,T>> {
 public:
